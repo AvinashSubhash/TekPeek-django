259daececaf7550e1510dabfaffc9e6cbcba0f0e
AvinashSubhash <avinashsubhash19@gmail.com>
2022-08-09 00:23:49 +0530

    Working-on-carousal-feature-incomplete

--- website/tekpeek/views.py
+++ website/tekpeek/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+def tekpeek(request):
+    html_page = loader.get_template('blog-page.html')
+    return HttpResponse(html_page.render())
