5ef2daf5d3a98e75a70a66dfe6948fe06f17660c
AvinashSubhash <avinashsubhash19@gmail.com>
2022-08-14 16:15:58 +0530

    template-integrated-and-modified

--- website/tekpeek/urls.py
+++ website/tekpeek/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.tekpeek,name='tekpeek')
+    path('',views.tekpeek,name='tekpeek'),
+    path('test/',views.blog_template,name="blog-template")
 ]
--- website/tekpeek/views.py
+++ website/tekpeek/views.py
@@ -6,3 +6,7 @@ from django.template import loader
 def tekpeek(request):
     html_page = loader.get_template('blog-page.html')
     return HttpResponse(html_page.render())
+
+def blog_template(request):
+    blog_page = loader.get_template('blog-template.html')
+    return HttpResponse(blog_page.render())
