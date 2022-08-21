f64e923d238de58599ce86de01203bb436b25095
AvinashSubhash <avinashsubhash19@gmail.com>
2022-08-21 19:16:57 +0530

    Added-database-and-loaded-data-to-blog-template

--- website/tekpeek/models.py
+++ website/tekpeek/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    blog_content = models.TextField(max_length=16777215)
+    blog_id = models.IntegerField()
--- website/tekpeek/views.py
+++ website/tekpeek/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from django.template import loader
+from . import models
+
 # Create your views here.
 def tekpeek(request):
     html_page = loader.get_template('blog-page.html')
+    print("Database contains: ")
     return HttpResponse(html_page.render())
 
 def blog_template(request):
+    collected_data = models.Data.objects.all()
+    collected_data = models.Data.objects.all()
     blog_page = loader.get_template('blog-template.html')
-    return HttpResponse(blog_page.render())
+    feed_data = {'data':collected_data,}
+    return HttpResponse(blog_page.render(feed_data))
