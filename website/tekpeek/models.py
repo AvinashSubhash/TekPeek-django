6f0db1fb091937828861364898e2ca116a01dd8f
AvinashSubhash <avinashsubhash19@gmail.com>
2022-08-22 11:09:45 +0530

    modified-table-structure

--- website/tekpeek/models.py
+++ website/tekpeek/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 class Data(models.Model):
     blog_content = models.TextField(max_length=16777215)
-    blog_id = models.IntegerField()
+    blog_title = models.TextField(max_length=65535)
+    blog_short = models.TextField(max_length=255)
+    blog_date = models.DateField('BLOG DATE',default=datetime.datetime.now)
+    blog_auth = models.TextField()
+    blog_image = models.TextField()
