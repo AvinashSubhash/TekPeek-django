9083701fd92f7c33df9aeffb05fa3eb3dc6aa967
AvinashSubhash <avinashsubhash19@gmail.com>
2022-08-26 17:10:41 +0530

    gave-some-final-finishes-to-blog-template-and-blog-page

--- website/homepage/admin.py
+++ website/homepage/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from tekpeek.models import Data,News
+admin.site.site_header = "TekPeek Admin Page"
+admin.site.index_title = "TekPeek Admin"
+admin.site.site_title = "Avinash"
 
 # Register your models here.
+
+
+@admin.register(Data)
+class DataAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
