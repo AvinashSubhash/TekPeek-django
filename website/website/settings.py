3628933b1842835c81859a98d3cd16e206435f50
AvinashSubhash <avinashsubhash19@gmail.com>
2022-08-29 12:18:42 +0530

    added-database-json

--- website/website/settings.py
+++ website/website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@&t(jk9%7iqpq0*+$b$g674%ejld6do4$mywy_&k9&(55n44s+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.47.34','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
